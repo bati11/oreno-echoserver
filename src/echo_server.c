6d22a159405d41edf46e44d02f34f3127c78c7a4
bati11 <mail.bati11@gmail.com>
2016-10-09 17:31:53 +0900

    add bind

--- src/echo_server.c
+++ src/echo_server.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <errno.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 
 void error(char *msg)
 {
@@ -16,6 +17,15 @@ int main(int argc, char *argv[])
   if (listener_d == -1) {
     error("socket err");
   }
+
+  struct sockaddr_in name;
+  name.sin_family = AF_INET;
+  name.sin_port = (in_port_t)htons(30000);
+  name.sin_addr.s_addr = htonl(INADDR_ANY);
+  if (bind(listener_d, (struct sockaddr *) &name, sizeof(name)) == -1) {
+    error("bind err");
+  }
+
   puts("wait...");
   while(1) {
   }
