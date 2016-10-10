289ee47d96f32be71a00b0f39d05e9c70fdfea5d
bati11 <mail.bati11@gmail.com>
2016-10-10 11:15:53 +0900

    add accept

--- src/echo_server.c
+++ src/echo_server.c
@@ -32,6 +32,12 @@ int main(int argc, char *argv[])
 
   puts("wait...");
   while(1) {
+    struct sockaddr_storage client_addr;
+    unsigned int address_size = sizeof(client_addr);
+    int connect_d = accept(listener_d, (struct sockaddr *)&client_addr, &address_size);
+    if (connect_d == -1) {
+      error("accept err");
+    }
   }
   return 0;
 }
