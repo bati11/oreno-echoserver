34068812d1a6c76ee68ba5a87397881ab3df7c18
bati11 <mail.bati11@gmail.com>
2016-10-15 23:21:22 +0900

    add signal handler

--- src/echo_server.c
+++ src/echo_server.c
@@ -5,11 +5,22 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
 
 void error(char *msg)
 {
   fprintf(stderr, "%s:%s\n", msg, strerror(errno));
   exit(1);
+}
+
+int catch_signal(int sig, int sa_flags, void (*handler)(int))
+{
+  struct sigaction action;
+  action.sa_handler = handler;
+  sigemptyset(&action.sa_mask);
+  action.sa_flags = sa_flags;
+  return sigaction(sig, &action, NULL);
 }
 
 int read_line(int socket, char *buf, int len)
@@ -28,8 +39,23 @@ int read_line(int socket, char *buf, int len)
   return len - slen;
 }
 
+void sig_chld(int signo)
+{
+  pid_t pid;
+  int stat;
+
+  while ( (pid = waitpid(-1, &stat, WNOHANG)) > 0) {
+    printf("child %d terminated\n", pid);
+  }
+  return;
+}
+
 int main(int argc, char *argv[])
 {
+  if (catch_signal(SIGCHLD, SA_RESTART, sig_chld) == -1) {
+    error("catch_signal err");
+  }
+
   int listener_d = socket(PF_INET, SOCK_STREAM, 0);
   if (listener_d == -1) {
     error("socket err");
@@ -54,8 +80,12 @@ int main(int argc, char *argv[])
     struct sockaddr_storage client_addr;
     unsigned int address_size = sizeof(client_addr);
     int connect_d = accept(listener_d, (struct sockaddr *)&client_addr, &address_size);
-    if (connect_d == -1) {
-      error("accept err");
+    if (connect_d < 0) {
+      if (errno == EINTR) {
+        continue;
+      } else {
+        error("accept err");
+      }
     }
 
     if (fork() == 0) {
